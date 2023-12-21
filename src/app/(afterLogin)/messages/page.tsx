cbff453838505c2ae4254c7ce0ed7010a31836fe
minseokiim <alstj991226@naver.com>
2023-12-22 04:08:07 +0900

    Feat: useRouter 사용

--- src/app/(afterLogin)/messages/page.tsx
+++ src/app/(afterLogin)/messages/page.tsx
@@ -3,47 +3,10 @@ import style from "./message.module.css";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import "dayjs/locale/ko";
+import  Room  from "./_component/Room";
 
 dayjs.locale("ko");
 dayjs.extend(relativeTime);
-
-function Room() {
-  const user = {
-    id: "hero",
-    nickname: "영웅",
-    Messages: [
-      {
-        content: "안녕하세요",
-        createdAt: new Date(),
-      },
-      {
-        content: "안녕하세요 22",
-        createdAt: new Date(),
-      },
-    ],
-  };
-
-  return (
-    <div className={style.room}>
-      <div className={style.roomUserImage}>
-        <img src={faker.image.avatar()} alt="" />
-      </div>
-      <div className={style.roomChatInfo}>
-        <div className={style.roomUserInfo}>
-          <b>{user.nickname}</b>&nbsp;
-          <span>@{user.id}</span>&nbsp;
-          <span className={style.postDate}>
-            {dayjs(user.Messages?.at(-1)?.createdAt).fromNow(true)}
-          </span>
-        </div>
-        <div className={style.roomLastChat}>
-          {user.Messages?.at(-1)?.content}
-          {/* at으로 마지막꺼 가져올 수 있음  */}
-        </div>
-      </div>
-    </div>
-  );
-}
 
 export default function Home() {
   return (
