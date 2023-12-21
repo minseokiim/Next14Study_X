32ebe9a65538c280971f41309cd49dc762dcb15f
minseokiim <alstj991226@naver.com>
2023-12-22 04:42:51 +0900

    Design: 반응형 추가

--- src/app/(afterLogin)/_component/Post.tsx
+++ src/app/(afterLogin)/_component/Post.tsx
@@ -7,6 +7,7 @@ import ActionButtons from "@/app/(afterLogin)/_component/ActionButtons";
 import Image from "next/image";
 import PostArticle from "./PostArticle";
 import { faker } from "@faker-js/faker";
+import PostImages from "./PostImages";
 
 dayjs.locale("ko");
 dayjs.extend(relativeTime);
@@ -29,10 +30,24 @@ export default function Post({ noImage }: Props) {
   };
 
   if (Math.random() > 0.5 && !noImage) {
-    target.Images.push({
-      imageId: 1,
-      link: faker.image.urlLoremFlickr(),
-    });
+    target.Images.push(
+      {
+        imageId: 1,
+        link: faker.image.urlLoremFlickr(),
+      },
+      {
+        imageId: 2,
+        link: faker.image.urlLoremFlickr(),
+      },
+      {
+        imageId: 3,
+        link: faker.image.urlLoremFlickr(),
+      },
+      {
+        imageId: 4,
+        link: faker.image.urlLoremFlickr(),
+      }
+    );
   }
 
   return (
@@ -61,15 +76,8 @@ export default function Post({ noImage }: Props) {
             </span>
           </div>
           <div>{target.content}</div>
-          <div className={style.postImageSection}>
-            {target.Images && target.Images.length > 0 && (
-              <Link
-                href={`/${target.User.id}/status/${target.postId}/photo/${target.Images[0].imageId}`}
-                className={style.postImageSection}
-              >
-                <img src={target.Images[0]?.link} alt="" />
-              </Link>
-            )}
+          <div>
+            <PostImages post={target} />
           </div>
           <ActionButtons />
         </div>
